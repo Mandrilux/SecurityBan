32e452078c8258c84ac9d494aeebe1f8ae19948b
heraud_b <baptiste.heraud@epitech.eu>
2017-12-02 22:47:14 +0100

    Fix bug

--- security.py
+++ security.py
@@ -3,12 +3,19 @@
 
 class Security:
     def __init__(self):
-        ips = []
-        flags = []
+        _ips = []
+        _flags = ["Failed password", "Invalid user"]
+        _lines = []
+
+
+    def extractIp(self, line):
+        return True
+
 
     """ Fonction qui permet de detecter un flag,
     Si le flag est present dans la string, la fonction
     retourne la valeur 1 , sinon elle retourne la valeur 0 """
+
 
     def detectFlag(self, s, key):
         if s.find(key) == -1:
@@ -16,8 +23,24 @@ class Security:
         else:
             return 1
 
+
     """ Cette fonction permet d'effectuer un controle sur
     les nouvelles lignes trouvées dans un fichier de log """
 
-    def ControlLines(self, lines):
-        print(lines)
+
+    def controlLines(self, lines):
+        self._lines = lines
+        for i in range(0, len(self._lines)):
+            print(self._lines[i])
+            if self.detectFlag(self._lines[i], "Failed password") == 1:
+                self.extractIp(lines[i])
+
+
+    """ Setter / getter  pour 'lines' """
+
+    def setLines(self, lines):
+        self._lines = lines
+
+    @property
+    def getLines(self):
+        return self._lines
